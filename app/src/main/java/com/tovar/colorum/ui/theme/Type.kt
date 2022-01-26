b4296885bb4372c535635420dcb465b1dbd6f20d
Uriel Tovar <ult_18@me.com>
2022-01-26 16:56:36 -0600

    :lipstick: Add custom font style

--- app/src/main/java/com/tovar/colorum/ui/theme/Type.kt
+++ app/src/main/java/com/tovar/colorum/ui/theme/Type.kt
@@ -2,27 +2,29 @@ package com.tovar.colorum.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tovar.colorum.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+	Font(resId = R.font.avenir_regular, weight = FontWeight.Normal),
+	Font(resId = R.font.avenir_medium, weight = FontWeight.Medium),
+	Font(resId = R.font.avenir_bold, weight = FontWeight.SemiBold)
+)
+
 val Typography = Typography(
 	body1 = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp
+		fontFamily = fonts,
+		fontSize = 14.sp
+	),
+	subtitle1 = TextStyle(
+		fontFamily = fonts,
+		fontWeight = FontWeight.Medium
+	),
+	h6 = TextStyle(
+		fontFamily = fonts,
+		fontWeight = FontWeight.SemiBold
 	)
-	/* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
