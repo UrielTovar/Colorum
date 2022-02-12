9218e379952e8ae1de92e87f495e483212c53d82
Uriel Tovar <ult_18@me.com>
2022-02-12 12:23:26 -0600

    :recycle: Rename variables following camel case structure

--- app/src/main/java/com/tovar/colorum/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/tovar/colorum/presentation/ui/theme/Theme.kt
@@ -1,11 +1,12 @@
 package com.tovar.colorum.presentation.ui.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val darkColorPalette = darkColors(
 	primary = scarpaFlow,
 	primaryVariant = charade,
 	secondary = Color.White
@@ -14,9 +15,9 @@ private val DarkColorPalette = darkColors(
 @Composable
 fun ColorumTheme(content: @Composable () -> Unit) {
 	MaterialTheme(
-		colors = DarkColorPalette,
-		typography = Typography,
-		shapes = Shapes,
+		colors = darkColorPalette,
+		typography = colorumTypography,
+		shapes = shapes,
 		content = content
 	)
 }
