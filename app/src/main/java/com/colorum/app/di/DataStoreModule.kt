1daa8309545b467b9abf07f42434f56d7c446220
Uriel Tovar <ult_18@me.com>
2022-02-22 21:24:29 -0600

    :art: Add context extensions file

--- app/src/main/java/com/colorum/app/di/DataStoreModule.kt
+++ app/src/main/java/com/colorum/app/di/DataStoreModule.kt
@@ -3,15 +3,13 @@ package com.colorum.app.di
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
+import com.colorum.app.presentation.common.extension.dataStorePreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore("user_preferences")
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -20,6 +18,6 @@ object DataStoreModule {
 	@Singleton
 	@Provides
 	fun providePreferences(@ApplicationContext context: Context): DataStore<Preferences> =
-		context.dataStore
+		context.dataStorePreferences
 	
 }
